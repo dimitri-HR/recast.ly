542bcc82ad93f8967e62af18b6b87e6805ae926e
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-02-25 19:09:35 +1100

    Start implementing youtube search

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 // TODO: Render the `App` component to the DOM
 
+// var App = () => (
+//   <div>
+//     <h2>My Grocery List</h2>
+//     <GroceryList todos={['cucumbers', 'kale']}/>
+//   </div>
+// );
+
+// ReactDOM.render(<App />, document.getElementById("actual-dom-element-where-I-want-to-render-my-component"));
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,35 @@
 var searchYouTube = (options, callback) => {
   // TODO
+
+  var url = 'https://www.googleapis.com/youtube/v3/search';
+
+// https://www.youtube.com/watch?v='videoId'
+
+  var searchTerm = 'testing';
+
+  options = {
+    key: YOUTUBE_API_KEY,
+    part: 'snippet',
+    type: 'video',
+    videoEmbeddable: true,
+    q: searchTerm
+  };
+
+  $.ajax({
+    url: url,
+    type: 'GET',
+    data: options,
+    // contentType: 'application/json',
+
+    success: function(data) {
+      console.log('YOUTUBE Success!');
+      console.log('Retrieved data -', data);
+    },
+    error: function(error) {
+      console.error('Failed to fetch videos', error);
+    }
+  });
+
 };
 
 window.searchYouTube = searchYouTube;
